f4544477cb6eea508107fa6fede97237cac4d559
김홍석 <drghdtjr@naver.com>
2025-07-06 23:51:59 +0900

    [chore] cicd 테스트

--- src/main/java/com/small/personal/domain/user/controller/UserController.java
+++ src/main/java/com/small/personal/domain/user/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Tag(name = "User", description = "회원관리 API")
+@Tag(name = "User", description = "회원관리 API CICD  테스트")
 @RequestMapping("/api/v1/users")
 @RestController
 @RequiredArgsConstructor
