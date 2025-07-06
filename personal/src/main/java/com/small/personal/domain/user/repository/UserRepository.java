bc3ddefb9db38bfe8523ad9d64ab54e17de4caa6
김홍석 <drghdtjr@naver.com>
2025-07-06 14:24:00 +0900

    [chore] cicd 테스트

--- personal/src/main/java/com/small/personal/domain/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.small.personal.domain.user.repository;
-
-import com.small.personal.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
---
+++ src/main/java/com/small/personal/domain/user/dto/request/UserRequestDto.java
@@ -0,0 +1,12 @@
+package com.small.personal.domain.user.dto.request;
+
+
+import lombok.Getter;
+
+@Getter
+public class UserRequestDto {
+
+  private String name;
+  private String password;
+
+}
